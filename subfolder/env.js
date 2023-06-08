c685db365539e09e63bcc129a2f492658812a655
unknown <andreyxdd@yandex.ru>
2023-06-08 17:34:23 -0600

    Updated dockerfile and scripts

--- subfolder/env.js
+++ subfolder/env.js
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 
-export const maxNumber = process.env.INPUT_MAX;
-export const minNumber = process.env.INPUT_MIN;
+export const maxNumber = process.env.MAX;
+export const minNumber = process.env.MIN;
