f2ca2c58aea28fc8c4c9f62edc69bb9a4a6d3d2e
unknown <andreyxdd@yandex.ru>
2023-06-08 17:26:18 -0600

    Updated dockerfile and scripts

--- subfolder/randomIntGenerator.js
+++ subfolder/randomIntGenerator.js
@@ -3,5 +3,8 @@ import random from 'random';
 // eslint-disable-next-line import/extensions
 import { maxNumber, minNumber } from './env.js';
 
+console.log('maxNumber',maxNumber)
+console.log('minNumber',minNumber)
+
 export const randomIntInARange = random.integer(+minNumber, +maxNumber);
 export const dummy = '';
