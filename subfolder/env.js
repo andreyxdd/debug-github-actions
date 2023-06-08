cedac49b33e5d0ec1c625a534ce5db44471554c4
unknown <andreyxdd@yandex.ru>
2023-06-08 16:44:12 -0600

    Added dockerfile and an action

--- subfolder/env.js
+++ subfolder/env.js
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 
-export const maxNumber = process.env.MAXIMUM_SUBFOLDER;
-export const minNumber = process.env.MINIMUM_SUBFOLDER;
+export const maxNumber = process.env.INPUT_MAX;
+export const minNumber = process.env.INPUT_MIN;
